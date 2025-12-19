f452063b45dcafc65c9ec259282dc9cfcac4c9fc
igonzalez <igonzalez@st.com.uy>
2025-12-19 17:36:11 -0300

    Fix typescript error: Export Pair interface

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,12 @@ export interface Person {
     name: string;
 }
 
+export interface Pair {
+    id: string;
+    person1: Person;
+    person2: Person;
+}
+
 export interface Match {
     id: string;
     team1: Person[];
